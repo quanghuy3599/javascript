ef50603cd6a9c8025d0348dda297abaf3b296d85
quanghuy3599 <gpsquanghuy11@gmail.com>
2020-07-31 21:12:49 +0700

    first commit

---
+++ var.js
@@ -0,0 +1,25 @@
+let ds=[
+    {
+        "Ten":"Quang Huy",
+        "Lop":"1"
+    },
+    {
+        "Ten":"Quang Huan",
+        "Lop":"2"
+    },
+    {
+        "Ten":"Quang Dang",
+        "Lop":"3"
+    },
+]
+
+ds.forEach(x=>{
+    console.log(x.Ten);
+    console.log(x.Lop);
+})
+
+console.log("TV");
+let a=5;
+let b=6;
+var kq=a+b;
+console.log(kq);
